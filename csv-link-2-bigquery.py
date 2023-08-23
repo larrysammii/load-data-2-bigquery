032896aff47af835edf008e934527cd42e0e2469
Larry Chan <larrylaren2@gmail.com>
2023-08-23 20:05:40 +0800

    Included a CSV to dataframe function, then clean up the hyphens so the data type would be registered.

--- csv-link-2-bigquery.py
+++ csv-link-2-bigquery.py
@@ -1,18 +1,21 @@
 import os
-import time
-import io
-import requests
+import pandas as pd
 from google.cloud import bigquery
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'credentials/hk-re-analytics-cfdf597ce9d5.json'
 
 csv_url = input('URL to CSV: ')
+df = pd.read_csv(csv_url)
+df = df.fillna('')
+df = df.replace('-', '0')
+print(df)
+
 client = bigquery.Client()
 
 project_dataset_id = input('project_id.dataset_id: ')
 table_id = input('Name the table id: ') # TODO: function to name it automatically
 
-full_table_id = f"{project_dataset_id}.{table_id}"
+full_table_id = f'{project_dataset_id}.{table_id}'
 
 job_config = bigquery.LoadJobConfig(
     source_format=bigquery.SourceFormat.CSV,
@@ -20,23 +23,16 @@ job_config = bigquery.LoadJobConfig(
     skip_leading_rows=1,
 )
 
-response = requests.get(csv_url)
-if response.status_code != 200:
-    print(response.reason)
-else:
-    content_byte = response.content
-    bytes_io = io.BytesIO(content_byte)
-    job = client.load_table_from_file(bytes_io, full_table_id, job_config=job_config)
+job = client.load_table_from_dataframe(
+    df, 
+    full_table_id, 
+    job_config=job_config
+    )
 
-    while job.state != 'DONE':
-        time.sleep(2)
-        job.reload()
-        print(job.state)
-    
-    print(job.result())
+job.result()
 
-    table = client.get_table(full_table_id)
-    print(
-        'Loaded {} rows and {} columns to {}'.format(
-            table.num_rows, len(table.schema), full_table_id
-    ))
+table = client.get_table(full_table_id)
+
+print(
+    f'Loaded {table.num_rows} rows and {len(table.schema)} columns to {full_table_id}'
+    )
